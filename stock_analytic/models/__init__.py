9ac459ffd1cc3705776aa1429edad3b1ffc9f30b
Yoshi Tashiro (Quartile) <tashiro@quartile.co>
2026-03-07 17:42:33 +0900

    [5889][ADD] stock_analytic (#9)

---
+++ stock_analytic/models/__init__.py
@@ -0,0 +1,8 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import analytic_applicability
+from . import stock_move
+from . import stock_picking
+from . import stock_rule
+from . import stock_scrap
+from . import stock_valuation_layer
