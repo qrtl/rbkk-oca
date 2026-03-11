b5cda0c361927f333976744828414062a8971b93
Aung Ko Ko Lin (Quartile) <45355704+AungKoKoLin1997@users.noreply.github.com>
2026-03-11 07:21:21 +0700

    [IMP] purchase_order_secondary_unit: reflect with oca (#40)

--- purchase_order_secondary_unit/models/__init__.py
+++ purchase_order_secondary_unit/models/__init__.py
@@ -3,5 +3,3 @@ from . import product_product
 from . import product_supplierinfo
 from . import product_template
 from . import purchase_order
-from . import res_company
-from . import res_config_settings
