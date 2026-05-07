dd4c6dbabe23377384cc1041146b1fffb2fefd0c
Aungkokolin1997 <lin@quartile.co>
2026-05-07 01:36:25 +0000

    reflect oca

--- mrp_stock_analytic/models/stock_move.py
+++ mrp_stock_analytic/models/stock_move.py
@@ -1,5 +1,5 @@
 # Copyright 2026 Quartile (https://www.quartile.co)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import api, models
 
@@ -10,14 +10,16 @@ class StockMove(models.Model):
     @api.model_create_multi
     def create(self, vals_list):
         for vals in vals_list:
-            # Set analytic distribution when adding a new component line
-            # after the MO is confirmed
-            if vals.get("raw_material_production_id") and not vals.get(
-                "analytic_distribution"
+            is_raw = vals.get("raw_material_production_id")
+            is_finished = vals.get("production_id")
+            production_id = is_raw or is_finished
+            if not production_id or vals.get("analytic_distribution"):
+                continue
+            production = self.env["mrp.production"].browse(production_id)
+            if not production.analytic_distribution:
+                continue
+            if is_raw or (
+                is_finished and production.company_id.mrp_analytic_on_finished
             ):
-                production = self.env["mrp.production"].browse(
-                    vals["raw_material_production_id"]
-                )
-                if production.analytic_distribution:
-                    vals["analytic_distribution"] = production.analytic_distribution
+                vals["analytic_distribution"] = production.analytic_distribution
         return super().create(vals_list)
